9968b6c94b104f15747af36f58710edbe6a66603
Konstantin Gunbin <konstantin.gunbin@agworld.com.au>
2015-10-26 09:14:43 +0800

    Removing sources from distr branch as they lead to asset build failure

--- src/addons/editors/index.js
+++
@@ -1,9 +0,0 @@
-/* @flow */
-var Editors = {
-  AutoComplete: require("./AutoCompleteEditor"),
-  DropDownEditor: require("./DropDownEditor"),
-  SimpleTextEditor: require("./SimpleTextEditor"),
-  CheckboxEditor: require("./CheckboxEditor")
-};
-
-module.exports = Editors;
--- src/addons/utils/isFunction.js
+++
@@ -1,9 +0,0 @@
-/* @flow */
-"use strict";
-
-var isFunction = function(functionToCheck: any): boolean{
-    var getType = {};
-    return functionToCheck && getType.toString.call(functionToCheck) === '[object Function]';
-}
-
-module.exports = isFunction;
