aa5127ffd4ee2d5a02ba8de8f7e46a4709a8ae4e
Guillaume Fourret <guillaume@Guillaumes-MacBook-Pro-2.local>
2015-10-28 12:30:04 +0800

    Bring back the original gulp tasks

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
 var del = require('del');
 gulp.task('watch', ['setWatch', 'browserSync'], function() {
-	gulp.watch('src/**', ['sources']);
-	//gulp.watch('examples/scripts/**', ['examples']);
+	//gulp.watch('themes/**', ['styles']);
+	gulp.watch('src/**', ['examples']);
 });
